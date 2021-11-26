eade71d5e64cd2c47652e230727632f3cde25e3b
vikysan77 <vikysan@outlook.com>
2021-11-26 12:56:58 +0300

    refactor: adjust CSS-formatting according to rule 3.7 in html-and-css.md

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='label-task label-completed';
+    label.className='label-task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
